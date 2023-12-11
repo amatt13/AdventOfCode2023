7316e68ca8c1f8124077d7b6dec1f36347b29592
Anders Lykke Matthiassen <anders.matthiassen@gmail.com>
2023-12-11 10:35:05 +0100

    Update day2_cube_conundrum.py
    The author of Advent of Code, has specifically requested people to not include the input strings. So I won't.

--- day2_cube_conundrum.py
+++ day2_cube_conundrum.py
@@ -3,11 +3,7 @@ import re
 
 from input_files.day2_input import day_2_input_string
 
-short_input_string = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+short_input_string = """The author of Advent of Code, has specifically requested people to not include the input strings. So I won't."""
 
 input_string = day_2_input_string
 
